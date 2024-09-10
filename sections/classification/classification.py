bb9163aa0e056373a3575a540349feb70ab923e4
JulienS <julien.salique@gmail.com>
2024-09-10 15:18:55 +0200

    last update 3

--- sections/classification/classification.py
+++ sections/classification/classification.py
@@ -3,7 +3,6 @@ from .functions import *
 import streamlit as st
 import pandas as pd
 from sklearn.metrics import classification_report
-
 from PIL import Image
 from sklearn.model_selection import train_test_split, GridSearchCV
 import os
